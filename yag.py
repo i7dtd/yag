20cc539d727d4a85482fe31f3392b560db7536d9
i7dtd <slack7@duck.com>
2026-04-11 01:50:49 +0300

    done parser, done init=]

--- yag.py
+++ yag.py
@@ -1,27 +1,33 @@
 import argparse
 import os
-
-
-
+import shutil
 
 
 
 def init(args):
+    if os.path.exists('.yag/refs/heads'):
+        shutil.rmtree('.yag/refs/heads')
+    if os.path.exists('.yag/objects'):
+        shutil.rmtree('.yag/objects')
     os.makedirs('.yag/refs/heads')
     os.mkdir('.yag/objects')
-    with open(".yag/HEAD") as file:
+    with open(".yag/HEAD", 'w') as file:
         file.write("ref: refs/heads/main")
 
 
+def add(args):
+    pass
+
 def createParser():
     parser = argparse.ArgumentParser(prog='yag')
-    subparsers = argparse.ArgumentParser(dest='command', requared=True)
+    subparsers = parser.add_subparsers(dest='command', required=True)
     
-    # init ?
-    parserInit = subparsers.add_subparser('init', help='Create an empty Yag repository or reinitialize an existing one')
+    
+    parserInit = subparsers.add_parser('init', help='Create an empty Yag repository or reinitialize an existing one')
 
     # add ? pass for 10 april
-    parserAdd = subparsers.add_subparsers('add', help='Add file contents to the index')
+    parserAdd = subparsers.add_parser('add', help='Add file contents to the index')
+    parserAdd.add_argument('file', help='Add file')
 
     return parser
 
@@ -29,6 +35,9 @@ def createParser():
 def main():
     parser = createParser()
     args = parser.parse_args()
+    
+    if args.command == 'init':
+        init(args)
 
 if __name__ == "__main__":
     main()
